2b40561a714792ed3eec64a24217a5c312a9d7e1
unknown <bao1310zz@gmail.com>
2026-03-13 08:56:05 +0700

    [add] lab02 railfence

--- lab02/api.py
+++ lab02/api.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from cipher.ceasar import CeasarCipher
 from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
 app = Flask(__name__)
 
 ceasar_cipher = CeasarCipher()
 vigenere_cipher = VigenereCipher()
+railfence_cipher = RailFenceCipher()
 
 @app.route("/api/ceasar/encrypt", methods=["POST"])
 def ceasar_encrypt():
@@ -37,6 +39,22 @@ def vigenere_decrypt():
     key = data['key']
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({'decrypted_massage': decrypted_text})
+
+@app.route('/api/railfence/encrypt', methods=["POST"])
+def encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/railfence/decrypt', methods=["POST"])
+def decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
